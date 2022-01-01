c1d62810a25ab13c53bd75d8ca16a097bb7e6ddb
Nebado <ruslan.nbd@gmail.com>
2022-01-01 20:08:10 +0100

    [bash] Add cut command

---
+++ bash/hackerrank/cut.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/sh
+
+# Cut 1
+cut -c 3
+
+# Cut 2
+cut -c 2,7
+
+# Cut 3
+cut -c 2-7
+
+# Cut 4
+cut -c 1-4
+
+# Cut 5
+cut -f '1-3'
+
+# Cut 6
+cut -c 13-
+
+# Cut 7
+cut -d ' ' -f 4
+
+# Cut 8
+cut -d ' ' -f 1-3
+
+# Cut 9
+cut -f 2-
