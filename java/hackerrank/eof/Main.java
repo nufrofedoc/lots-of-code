a268366ab21f2b9cda64dd2769ac3810306a2147
Nebado <ruslan.nbd@gmail.com>
2022-05-02 19:59:56 +0300

    [java] add hackerrank solutions

---
+++ java/hackerrank/eof/Main.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int counter = 1;
+        
+        while (sc.hasNext()) {
+            System.out.println(counter + " " + sc.nextLine());
+            counter++;
+        }
+    }
+}
