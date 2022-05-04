4ab30805fb115b8edbb1bbdea8b03ae6b9f640cd
Nebado <ruslan.nbd@gmail.com>
2022-05-04 21:02:48 +0300

    [java] add hackerrank solutions

---
+++ java/hackerrank/currency/Main.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+        String us, india, china, france;
+        us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        india = NumberFormat.getCurrencyInstance(new Locale("en", "IN")).format(payment);
+        china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+                
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
